3c8159ea1077216b1fa34ecf5f2924d210a585b5
OllyDorvelus <ollydorvelus@gmail.com>
2018-04-18 19:59:35 -0400

    amazon s3 bucket support

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 #   process resize_to_limit: [300, 450]
   # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -15,6 +15,7 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
